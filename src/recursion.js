7a9fae244f8808282906e93be0c5c96c91f5dd72
Dwayne <deandraper82@gmail.com>
2020-08-12 06:05:05 -0500

    Solutions for a few more recursion problems

--- src/recursion.js
+++ src/recursion.js
@@ -62,11 +62,34 @@ var isEven = function(n) {
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+  let isNeg = false;
+  if (n < 0) {
+    n = Math.abs(n);
+    isNeg = true;
+  }
+  if (n === 0) {
+    return 0;
+  }
+  if (!isNeg) {
+    return n - 1 + sumBelow(n - 1);
+  } else {
+    return -1 * (n - 1 + sumBelow(n - 1));
+  }
 };
 
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+  // let results = [];
+  // if (arguments[0] === arguments[1]) {
+  //   results.push(arguments[0]);
+  //   return results;
+  // } else {
+  //   results.push(arguments[0]);
+  //   return range(x+1, y)
+  // }
+
+
 };
 
 // 7. Compute the exponent of a number.
